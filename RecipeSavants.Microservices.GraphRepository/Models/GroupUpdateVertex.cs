3044868137663017755d5a2fad894d6221ea5aa8
RecipeSavants <32582201+RecipeSavants@users.noreply.github.com>
2019-06-19 09:38:12 -0500

    Create GroupUpdateVertex.cs

---
+++ RecipeSavants.Microservices.GraphRepository/Models/GroupUpdateVertex.cs
@@ -0,0 +1,10 @@
+public class GroupUpdteVertex
+{
+        public string id {get;set;}
+        public DateTime TimeStamp { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public UpdateType UpdateType { get; set; }
+        public List<string> Url { get; set; }
+        public List<string> ImageUrl { get; set; }
+}
