a753f9be0848043b1e6b7410fb6bc07a14985097
RecipeSavants <32582201+RecipeSavants@users.noreply.github.com>
2019-06-19 09:36:44 -0500

    Create GroupVertex.cs

---
+++ RecipeSavants.Microservices.GraphRepository/Models/GroupVertex.cs
@@ -0,0 +1,9 @@
+public class GroupVertex
+{
+  public string id {get;set;}
+  public string Name {get;set;}
+  public List<string> FocusTags {get;set;}
+  public string BackgroundPhotoUrl {get;set;}
+  public string Description {get;set;}
+  public bool IsPublic {get;set;}
+}
