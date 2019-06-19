cb6146182df61cf72cca2e64bd02d13f71e68690
RecipeSavants <32582201+RecipeSavants@users.noreply.github.com>
2019-06-19 09:42:28 -0500

    Update GroupUpdateVertex.cs

--- RecipeSavants.Microservices.GraphRepository/Models/GroupUpdateVertex.cs
+++ RecipeSavants.Microservices.GraphRepository/Models/GroupUpdateVertex.cs
@@ -1,6 +1,7 @@
 public class GroupUpdteVertex
 {
         public string id {get;set;}
+        public string GroupId {get;set;}
         public DateTime TimeStamp { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
