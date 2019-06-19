3358341cb6732bbe1d347d17d984de3aa73c1777
RecipeSavants <32582201+RecipeSavants@users.noreply.github.com>
2019-06-19 09:39:03 -0500

    Create GroupUpdateCommentVertex.cs

---
+++ RecipeSavants.Microservices.GraphRepository/Models/GroupUpdateCommentVertex.cs
@@ -0,0 +1,8 @@
+public class GroupUpdateCommentVertex
+{
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public List<string> ImageUrl { get; set; }
+        public DateTime TimeStamp { get; set; }
+}
